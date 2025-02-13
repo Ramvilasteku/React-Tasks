a6e174e4358d684edcf411a0dd721440d7e0b810
Ramvilasteku <ramvilasteku@gmail.com>
2025-02-13 21:43:15 +0530

    new

--- react-task3/src/App.js
+++ react-task3/src/App.js
@@ -2,9 +2,18 @@ import React from "react";
 
 class App extends React.Component{
 render(){
-  return(
+  const data=[{id:1,m_Name:'Pushpa2',m_Budget:'450CR'},{id:1,m_Name:'Devara',m_Budget:'300CR'},{id:1,m_Name:'Salaar',m_Budget:'270CR'}]
+
+return(
 <>
-<h1>Hello</h1>
+<h1>Movie Name with Budget</h1>
+
+  {data.map(x=>{
+    return(
+      <Child/>
+    )
+
+  })}
 
 </>
 
