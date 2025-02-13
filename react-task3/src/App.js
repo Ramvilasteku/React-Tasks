1f6db10a812a26bc738985d48e9cc5f33c7a0141
Ramvilasteku <ramvilasteku@gmail.com>
2025-02-14 00:20:28 +0530

    new

--- react-task3/src/App.js
+++ react-task3/src/App.js
@@ -1,25 +1,101 @@
-import React from "react";
+// import React from "react";
+// import Child  from "./Child";
 
-class App extends React.Component{
-render(){
-  const data=[{id:1,m_Name:'Pushpa2',m_Budget:'450CR'},{id:1,m_Name:'Devara',m_Budget:'300CR'},{id:1,m_Name:'Salaar',m_Budget:'270CR'}]
+// class App extends React.Component{
+// render(){
+//   const data=[{id:1,m_Name:'Pushpa2',m_Budget:'450CR'},{id:1,m_Name:'Devara',m_Budget:'300CR'},{id:1,m_Name:'Salaar',m_Budget:'270CR'}]
 
-return(
-<>
-<h1>Movie Name with Budget</h1>
+// return(
+// <div>
+// <h1>Movie Name with Budget</h1>
 
-  {data.map(x=>{
-    return(
-      <Child/>
-    )
+//   {data.map(x=>{
+//     return(
+//       <Child m_n={x.m_Name} m_b={x.m_Budget}/>
+//     )
 
-  })}
+//   })}
 
-</>
+// </div>
 
-  )
+//   )
 
+// }
+// }
+
+// export default App
+
+
+// import React from "react";
+
+// import './App.css'
+
+// class App extends React.Component{
+
+//   constructor(){
+//       super()
+//       this.state={
+//           count:0
+//         }
+//       }
+
+//       dec=()=>{
+//           this.setState({
+//               count:this.state.count-1
+//             })
+//           }
+
+//           inc=()=>{
+//               this.setState({
+//       count:this.state.count+1
+//     })
+//   }
+//   render(){
+//       return(
+//           <div className="main">
+//           <button onClick={this.dec}>-</button>
+
+//         <h1>{this.state.count}</h1>
+
+//           <button onClick={this.inc}>+</button>
+//           </div>
+//         )
+//       }
+
+
+//     }
+
+//     export default App
+
+
+
+import React from 'react';
+import './App.css'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      color: 'white'
+    }
+  }
+
+
+
+  render() {
+    return (
+        <div className='main'  >
+          <div className='circle' style={{ backgroundColor: this.state.color }}></div>
+          <div id='btn'>
+            <button onClick={() => this.setState({ color: this.state.color = "red" })}>Stop</button>
+            <button onClick={() => this.setState({ color: this.state.color = "yellow" })}>Ready</button>
+            <button onClick={() => this.setState({ color: this.state.color = "green" })}>Go</button>
+          </div>
+
+        </div>
+        )
+  }
 }
-}
 
-export default App
+        export default App
+
---
+++ react-task3/src/Child.js
@@ -0,0 +1,16 @@
+// import React from "react";
+
+
+// class Child extends React.Component{
+//     render(){
+//         console.log(this.props,"child comp")
+//         return(
+//             <div>
+//               <h1>{this.props.m_n}</h1>  
+//               <h3>{this.props.m_b}</h3>  
+//             </div>
+//         )
+//     }
+// }
+
+// export default Child
